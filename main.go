a52d91a57cf3143a59297f2af9186cb7c584fe36
mrLSD <mrlsd@ya.ru>
2016-03-26 15:49:42 +0000

    #1 lessons

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "./utils"
+)
+
+func main() {
+    fmt.Printf("Arr: %v\n", arrays())
+    
+    utils.ForTest()
+    utils.TestArrays()
+    utils.TestSLices()
+    utils.TestMaps()
+    utils.TestVect()
+}
+
+// Test simple arrays
+func arrays() [3]int {
+    var ar1 [3]int
+    // var ar2 [...]int
+    //ar1[0] = 3
+
+    // ar2[1] = 2
+    return ar1
+}
